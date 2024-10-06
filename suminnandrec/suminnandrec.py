b73cc4802f36f027316d51786b5e951436afc23b
yahir <yahir@LAPTOP-H2INCEOF>
2024-10-06 16:23:08 -0600

    Agregar archivos de proyecto.

---
+++ suminnandrec/suminnandrec.py
@@ -0,0 +1,26 @@
+print("Este programa es capaz de sumar todos los numeros ")
+
+Numero = int(input("Anote los numeros que desea sumar:   "))
+
+def sumainterativa(n):
+    suma = 0
+
+    while n > 9:
+        suma += n % 10
+        n //= 10
+    return suma + n
+
+def sumarecursiva(n):
+    if n <= 9:
+        return n
+    else:
+        return sumarecursiva(n // 10) + n % 10
+    
+print(f"El resultado de la suma iterativa es: {sumainterativa(Numero)}")
+print(f"El resultado de la suma recursiva es: {sumarecursiva(Numero)}")
+
+
+
+
+
+
